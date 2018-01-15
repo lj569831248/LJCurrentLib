a99f4f87c062449df9c8f474a52ead075c90c688
lj569831248 <569831248@qq.com>
2018-01-15 20:24:17 +0800

    添加 request

--- Tools/MyRequest.h
+++ Tools/MyRequest.h
@@ -9,5 +9,9 @@
 #import <Foundation/Foundation.h>
 
 @interface MyRequest : NSObject
-+ (void)requestGetWithUrl:(NSString *)urlString callback:(void (^)(NSURLResponse *response, id responseObject, NSError *error))callback;
+//+ (void)requestGetWithUrl:(NSString *)urlString callback:(void (^)(NSURLResponse *response, id responseObject, NSError *error))callback;
+
++ (void)getRequestWithURL:(NSString *)URLString  delegate:(id <NSURLSessionDelegate>)delegate completionHandler:(void (^)(NSData * data, NSURLResponse *  response, NSError * error))completionHandler;
+
++ (void)postRequestWithURL:(NSString *)URLString param:(NSData *)paramData  delegate:( id <NSURLSessionDelegate>)delegate completionHandler:(void (^)(NSData *  data, NSURLResponse *  response, NSError *  error))completionHandler;
 @end
--- Tools/MyRequest.m
+++ Tools/MyRequest.m
@@ -7,17 +7,50 @@
 //
 
 #import "MyRequest.h"
-#import <AFNetworking/AFNetworking.h>
+//#import <AFNetworking/AFNetworking.h>
 @implementation MyRequest
-+ (void)requestGetWithUrl:(NSString *)urlString callback:(void (^)(NSURLResponse *response, id responseObject, NSError *error))callback{
-    
-//    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-//    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
-    NSMutableURLRequest *request  = [[AFJSONRequestSerializer serializer] requestWithMethod:@"GET" URLString:urlString parameters:nil error:nil];
-    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/plain",@"text/html", nil];
-    NSURLSessionDataTask *dataTask = [manager dataTaskWithRequest:request completionHandler:callback];
+//+ (void)requestGetWithUrl:(NSString *)urlString callback:(void (^)(NSURLResponse *response, id responseObject, NSError *error))callback{
+//    
+////    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+////    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+//    NSMutableURLRequest *request  = [[AFJSONRequestSerializer serializer] requestWithMethod:@"GET" URLString:urlString parameters:nil error:nil];
+//    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+//    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/plain",@"text/html", nil];
+//    NSURLSessionDataTask *dataTask = [manager dataTaskWithRequest:request completionHandler:callback];
+//    [dataTask resume];
+//}
+
+//Post 请求
++ (void)postRequestWithURL:(NSString *)URLString param:(NSData *)paramData  delegate:( id <NSURLSessionDelegate>)delegate completionHandler:(void (^)(NSData *  data, NSURLResponse *  response, NSError *  error))completionHandler{
+    NSURL *url = [NSURL URLWithString:URLString];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:paramData];
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config delegate:delegate delegateQueue:queue];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            SAFE_BLOCK(completionHandler,data,response,error);
+            [session finishTasksAndInvalidate];
+        });
+    }];
     [dataTask resume];
-     
+}
+
+//Get 请求
++ (void)getRequestWithURL:(NSString *)URLString  delegate:(id <NSURLSessionDelegate>)delegate completionHandler:(void (^)(NSData * data, NSURLResponse *  response, NSError * error))completionHandler{
+    NSURL *url = [NSURL URLWithString:URLString];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config delegate:delegate delegateQueue:queue];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            SAFE_BLOCK(completionHandler,data,response,error);
+            [session finishTasksAndInvalidate];
+        });
+    }];
+    [dataTask resume];
 }
 @end
